d4210b9f9060f77dad2d85fdd8fae64d56da1b5a
elishatofunmi <elishatofunmi@gmail.com>
2020-04-07 22:07:50 -0400

    commit

---
+++ c++ programs/factor.cpp
@@ -0,0 +1,11 @@
+// C++ Program to find factorial of a number
+// Factorial on n = 1*2*3*...*n
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    printf('Hello world\n');
+    return 0;
+}
