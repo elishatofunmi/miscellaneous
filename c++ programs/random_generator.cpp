557f73aedee0c827d4f4394fe9649198db68c001
elishatofunmi <elishatofunmi@gmail.com>
2020-04-25 01:31:01 -0400

    commit

--- c++ programs/random_generator.cpp
+++ c++ programs/random_generator.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+    srand(time(0));
     for (int x= 0; x<25; x++){
         cout << 1+ (rand()%6)<<endl;
     }
