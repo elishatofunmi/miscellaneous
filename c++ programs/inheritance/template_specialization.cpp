63841122d162e84da836f330827579f194659a60
elishatofunmi <elishatofunmi@gmail.com>
2020-04-30 05:20:45 -0400

    commit

--- c++ programs/inheritance/template_specialization.cpp
+++ c++ programs/inheritance/template_specialization.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+//generic class
+template <class T>
+class Spunky{
+    public:
+        Spunky(T x){
+            cout<<x<<" is not a character!"<<endl;
+        }
+};
+
+
+
+// specific class (specialized class)
+template<>
+class Spunky<char>{
+    public:
+        Spunky(char x){
+            cout<<x<<" is indeed a character"<<endl;
+        }
+};
 
 int main(){
-    
+    int x= 53;
+    Spunky <char> spu('q');
+    Spunky <double> spu1(45.2);
+    Spunky <int> spu2(x);
 }
